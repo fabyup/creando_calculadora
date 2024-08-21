bc2d4d6ef7ef0390669cdce4a866a694a4360a0c
fabyup <afabyblasco@gmail.com>
2024-08-21 15:18:15 -0300

    se agrego la accion de permitir un solo operador,todavia sin terminar

--- codigo.js
+++ codigo.js
@@ -1,19 +1,28 @@
 const pantalla = document.querySelector("input");
-let noAgregarMasDeUnOperador = true;
+const OPERADORES = ["+", "-", "*", "/", "%"];
+const NUMEROS = "0123456789";
 
 function actualizarPantalla(input) {
+  let ultimoCaracter = pantalla.value[pantalla.value.length + -1];
   if (input.innerHTML === "=") {
     pantalla.value = eval(pantalla.value);
   } else {
     pantalla.value += input.innerHTML;
   }
+
+  if (
+    !OPERADORES.includes(ultimoCaracter) ||
+    NUMEROS.includes(input.innerHTML)
+  ) {
+  } else {
+    alert("opcion no permitida");
+  }
 }
+
 function borrarPantalla() {
-  pantalla.value = "";
+  pantalla.value = " ";
 }
 
 function borrarUnCaracter() {
-  pantalla.value = pantalla.value.slice(0,-1);
+  pantalla.value = pantalla.value.slice(0, -1);
 }
-
-
