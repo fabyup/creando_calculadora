d29dc17f35013f019f596248c7c81de2423e8333
D-5 <>
2024-08-23 00:10:55 +0200

    se cambio el codigo a switch

--- codigo.js
+++ codigo.js
@@ -1,29 +1,45 @@
 const pantalla = document.querySelector("input");
 const OPERADORES = ["+", "-", "*", "/", "%"];
 const NUMEROS = "0123456789";
+const OPCION_RESULTADO = "=";
+const OPCION_LIMPIAR = "c";
+const PANTALLA_VACIA = " ";
+const BORRAR_ULTIMO = "←";
 
 function actualizarPantalla(input) {
-  let ultimoCaracter = pantalla.value[pantalla.value.length + -1];
-  
+  let ultimoCaracter = pantalla.value[pantalla.value.length -1];
+  if (!OPERADORES.includes(ultimoCaracter) || NUMEROS.includes(input.innerHTML)) {
 
-  if (
-    !OPERADORES.includes(ultimoCaracter) ||
-    NUMEROS.includes(input.innerHTML)
-  ) {
-    if (input.innerHTML === "=") {
-      pantalla.value = eval(pantalla.value);
-    } else {
-      pantalla.value += input.innerHTML;
+    switch (input.innerHTML) {
+      case OPCION_RESULTADO:
+        pantalla.value = eval(pantalla.value);
+        break;
+
+      case OPCION_LIMPIAR:
+        pantalla.value = PANTALLA_VACIA;
+        break;
+
+      case BORRAR_ULTIMO:
+        pantalla.value = pantalla.value.slice(0, -1);
+        break;
+       default:
+        pantalla.value += input.innerHTML;
+        break;
     }
-  } else {
-    alert("opcion no permitida");
+
+        }else{
+          alert("OPCION NO PERMITIDA")
   }
-}
+}  
+    
 
-function borrarPantalla() {
-  pantalla.value = " ";
-}
 
-function borrarUnCaracter() {
-  pantalla.value = pantalla.value.slice(0, -1);
-}
+ 
+
+
+
+
+
+
+
+
